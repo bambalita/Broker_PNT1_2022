c123b33259b19eb5dd3f34f7b2b317fca0ada509
Guibert <34744071@comunidadort.edu.ar>
2022-11-03 20:44:55 -0300

    Creacion de las clases Direccion y Persona

---
+++ Broker/Models/Direccion.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Broker.Models
+{
+    public class Direccion
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Pais { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Provincia { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string  Ciudad { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Calle { get; set; }
+        [Required]
+        public int Numero { get; set; }
+        
+        public string Departamento { get; set; }
+    }
+}
---
+++ Broker/Models/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Broker.Models
+{
+    public class Persona
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Apellido { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Mail { get; set; }
+
+        [ForeignKey("Direccion")]
+        public Direccion Direccion { get; set; }
+
+        [RegularExpression(@"[0-9]{2}[0-9]{4}[0-9]{4}")]
+        public string Telefono { get; set; }
+
+        [Required]
+        [Range(1,100000000, ErrorMessage = "El valor debe estar entre 1 y 100000000")]
+        public long DNI { get; set; }
+
+    }
+}
